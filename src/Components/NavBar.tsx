ff70532bb90d59c946888b8e2d6b0939575c4c6c
GriffinAB <griffinadjei@gmail.com>
2024-08-31 22:10:39 +0000

    Displaying critic score

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
       <ColorModeSwitch />
-    </HStack> //HStack is a short form of horizontal stack. It is from Chakra UI
+    </HStack> //HStack is a short form of horizontal stack. It is from Chakra UI. Also boxsize gives us the size of the box inwhich the logo must fit in.
   );
 };
 
