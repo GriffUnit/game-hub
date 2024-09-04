73768327be4b027d5d4c615251d0e5f44e72726f
GriffinAB <griffinadjei@gmail.com>
2024-09-04 18:50:52 +0000

    Build search input

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack> //HStack is a short form of horizontal stack. It is from Chakra UI. Also boxsize gives us the size of the box inwhich the logo must fit in.
   );
