252afc413709e48e4ad9edcce0d124fbfadcbc18
GriffinAB <griffinadjei@gmail.com>
2024-09-04 20:21:05 +0000

    Implement searching

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack> //HStack is a short form of horizontal stack. It is from Chakra UI. Also boxsize gives us the size of the box inwhich the logo must fit in.
   );
