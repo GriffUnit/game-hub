ad3f0f461683f990585e5b4013cae4e8d54c5e5e
GriffinAB <griffinadjei@gmail.com>
2024-09-05 23:34:20 +0000

    Update page title

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -23,9 +23,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
   if (error) return null;
-  //if (isLoading) return <Spinner />
-  if (isLoading)
-    return skeletons.map((skeleton) => <GenreSkeleton key={skeleton} />);
+  if (isLoading) return <Spinner />;
 
   return (
     <>
